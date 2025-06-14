1032c3845b6d8caf31ab0c16dcefe4c7ff7f0cc8
thuber19 <52424519+thuber19@users.noreply.github.com>
2025-06-14 08:11:59 +0200

    Update calculators.py

--- Rec-Sys-Challenge-Github-Repo/custom_pipline/calculators.py
+++ Rec-Sys-Challenge-Github-Repo/custom_pipline/calculators.py
@@ -420,7 +420,8 @@ class CombinedCalculator(Calculator):
 
     @property
     def features_size(self) -> int:
-        print(f"Calculator: {type(calc).__name__} + feature_size: {calc.features_size}" for calc in self._calculators)
+        for calc in self._calculators:
+            print(f"Calculator: {type(calc).__name__} + feature_size: {calc.features_size}")
         return sum(calc.features_size for calc in self._calculators)
 
     def compute_features(self, events: pd.DataFrame) -> np.ndarray:
