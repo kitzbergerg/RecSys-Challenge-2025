14b2f3f8cf49eccf3ac4d3bbf24ec58869420965
thuber19 <52424519+thuber19@users.noreply.github.com>
2025-06-14 08:18:40 +0200

    Update calculators.py

--- Rec-Sys-Challenge-Github-Repo/custom_pipline/calculators.py
+++ Rec-Sys-Challenge-Github-Repo/custom_pipline/calculators.py
@@ -205,7 +205,7 @@ class PriceStatsCalculator(Calculator):
         if prices.empty:
             return np.zeros(4, dtype=EMBEDDINGS_DTYPE)
         
-        print(f"{prices.mean()=}, {prices.min()=}, {prices.max()=}, {prices.std() if len(events) > 1 else 0.0}")
+        #print(f"{prices.mean()=}, {prices.min()=}, {prices.max()=}, {prices.std() if len(events) > 1 else 0.0}")
         return np.array([
             prices.mean(),
             prices.min(),
