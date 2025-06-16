dd3d7b0481a65bee45b1fa7e27abfa5ccf20e5c1
kitzbergerg <gabriel.kitzberger@gmail.com>
2025-06-16 09:47:37 +0200

    create concat of embeddings

---
+++ Rec-Sys-Challenge-Github-Repo/embeddings_transformer/concat_embeddings.py
@@ -0,0 +1,40 @@
+import numpy as np
+import torch
+
+client_ids = np.load("../results/transformer/v1/client_ids.npy")
+embeddings = np.load("../results/transformer/v1/embeddings.npy")
+
+client_ids_other = np.load("../results/vinzent/client_ids.npy")
+embeddings_other = np.load("../results/vinzent/embeddings.npy")
+
+print(client_ids.shape)
+print(embeddings.shape)
+print(client_ids_other.shape)
+print(embeddings_other.shape)
+
+other_dict = {cid: emb for cid, emb in zip(client_ids_other, embeddings_other)}
+
+client_ids_concat = []
+embeddings_concat = []
+
+for cid, emb in zip(client_ids, embeddings):
+    if cid in other_dict:
+        client_ids_concat.append(cid)
+        embeddings_concat.append(np.concat((emb, other_dict[cid])))
+    else:
+        print("WARNING: client_id {} not in other_dict".format(cid))
+        exit(0)
+
+client_ids_concat = np.array(client_ids_concat)
+embeddings_concat = np.stack(embeddings_concat)
+
+print(client_ids_concat.shape)
+print(embeddings_concat.shape)
+assert client_ids.shape == client_ids_concat.shape
+assert embeddings.shape[0] == embeddings_concat.shape[0]
+assert client_ids_concat.shape[0] == embeddings_concat.shape[0]
+assert embeddings.shape[1] + embeddings_other.shape[1] == embeddings_concat.shape[1]
+
+# Save the aligned client_ids and combined embeddings
+np.save("../results/transformer/v1/combined/client_ids.npy", client_ids_concat)
+np.save("../results/transformer/v1/combined/embeddings.npy", embeddings_concat)
