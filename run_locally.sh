da86568d7605a09a63c21aca78ce506aa036b4eb
thuber19 <52424519+thuber19@users.noreply.github.com>
2025-06-17 17:17:35 +0200

    Update run_locally.sh

--- run_locally.sh
+++ run_locally.sh
@@ -20,4 +20,4 @@ echo "Running calculators pipeline with split data..."
 python -m custom_pipline.create_embeddings --data-dir /home/jovyan/shared/194.035-2025S/data/group_project/data_new/ --embeddings-dir ../embeddings_split --split
 
 echo "Running training pipeline with split data..."
-python -m training_pipeline.train --data-dir /home/jovyan/shared/194.035-2025S/data/group_project/data --embeddings-dir ../embeddings_split --tasks churn propensity_category propensity_sku --log-name exp_20250401 --accelerator gpu --devices auto --disable-relevant-clients-check --score-dir ../scores --neptune-project /home/jovyan/groups/194.035-2025S/Group_36/Group_36/neptune
+python -m training_pipeline.train --data-dir /home/jovyan/shared/194.035-2025S/data/group_project/data_new/ --embeddings-dir ../embeddings_split --tasks churn propensity_category propensity_sku --log-name exp_20250401 --accelerator gpu --devices auto --disable-relevant-clients-check --score-dir ../scores --neptune-project /home/jovyan/groups/194.035-2025S/Group_36/Group_36/neptune
